d9e4f6b45489c68b46264af1789010bbc0d6bc02
fahd ahsyani <fahdahsayni@gmail.com>
2023-03-31 12:14:03 +0200

    fixed Api

--- src/utils/sanity-client.js
+++ src/utils/sanity-client.js
@@ -1,9 +1,9 @@
 import { createClient } from "next-sanity";
 
-export const projectId = process.env.NEXT_PUBLIC_SANITY_PROJECT_ID;
-export const dataset = process.env.NEXT_PUBLIC_SANITY_DATASET; // "production"
-export const apiVersion = process.env.NEXT_PUBLIC_SANITY_API_VERSION; // "2023-01-01"
-export const token = process.env.NEXT_PUBLIC_SANITY_TOKEN;
+export const projectId = "elgcoxli";
+export const dataset = "production"
+export const apiVersion = "2023-01-01"
+export const token = "skRyz05xfa5nN0LpfK8B5CAvsUIt9Yq2TJ8JBeaQmNpsMARCaIpiiOo2JyKT99ojtxSZ7YB10BHEVZxwtGULfnpaHaoWsXX0q6a4WUxrUxCkQnXdg8GP0QvSvepXLTDAqjoo85cIyNsbFbPi27PXHjuNQ0VLi9AMNQCQRyGXwXsi8Phwh4CH";
 
 export const client = createClient({
   projectId,
